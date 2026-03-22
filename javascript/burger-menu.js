208a065c97debd33690c5793af842cd56283eced
lauhofn-mmd <lauhofn@gmail.com>
2026-03-22 20:07:40 +0100

    Opdateret burger menu

---
+++ javascript/burger-menu.js
@@ -0,0 +1,9 @@
+
+function mobileMenu() {
+  var x = document.getElementById("menuLinks");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+} 
