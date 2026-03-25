b2193d73de1de304f9f19f78ac30225d1d60c633
ThatOneGemini <sifegjensen@gmail.com>
2026-03-25 12:20:12 +0100

    dropdown test

--- javascript/burger-menu.js
+++ javascript/burger-menu.js
@@ -14,13 +14,13 @@ function myFunction(){
 }
 
 window.onclick = function(event){
-    if (!event.target.matches(".dropbtn")) {
+    if (!event.target.matches('.dropbtn')) {
         var dropdowns = this.document.getElementByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns [i];
-            if (openDropdown.classList.contains("show")){
-                openDropdown.classList.remove("show");
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
             }
         }
     }
